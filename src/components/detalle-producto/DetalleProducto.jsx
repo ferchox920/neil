3250522998c9f700cfddf93e8c37d350202124c8
ferchox920 <fernandoramones92@gmail.com>
2023-09-18 23:26:52 -0300

    refactor

--- src/components/detalle-producto/DetalleProducto.jsx
+++ src/components/detalle-producto/DetalleProducto.jsx
@@ -1,7 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 export const DetalleProducto = () => {
+  const { producto } = useParams(); // Obtiene los parámetros de la URL
+  const [product, setProduct] = useState({});
+
+  useEffect(() => {
+    const fetchProductoDetalle = async () => {
+      try {
+        const response = await fetch(
+          `http://localhost:3000/product/${producto}`
+        );
+        const data = await response.json();
+        console.log(data);
+        setProduct(data);
+      } catch (error) {
+        console.error("Error en la solicitud:", error);
+      }
+    };
+
+    fetchProductoDetalle();
+  }, [setProduct]);
   return (
-    <div>DetalleProducto</div>
-  )
-}
+    <div>
+      <h2>Detalle del Producto</h2>
+      {product && (
+        <div>
+          <h3>Detalles del Producto</h3>
+          <img src={product.imageProfile} alt="asdas" srcset="" />
+
+          <p>Nombre: {product.name}</p>
+          <p>Precio: ${product.price}</p>
+        </div>
+      )}
+    </div>
+  );
+};
--- src/views/detalle-producto/DetalleProducto.jsx
+++ src/views/detalle-producto/DetalleProducto.jsx
@@ -1,43 +1,9 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-
+import { DetalleProducto } from "../../components/detalle-producto/DetalleProducto";
 
 export const DetalleProductoView = () => {
-  const { categoria, producto } = useParams(); // Obtiene los parámetros de la URL
-  const [product, setProduct] =useState({})
-
-
-  useEffect(() => {
-    const fetchProductoDetalle = async () => {
-      try {
-        const response = await fetch(`http://localhost:3000/product/${producto}`);
-        const data = await response.json();
-        console.log(data);
-        setProduct(data);
-      } catch (error) {
-        console.error("Error en la solicitud:", error);
-      }
-    };
-
-    fetchProductoDetalle();
-  }, [ setProduct]);
-
-  console.log('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA');
-  console.log(product);
   return (
     <div>
-      <h2>Detalle del Producto</h2>
- 
-      {product && (
-        <div>
-          <h3>Detalles del Producto</h3>
-          <img src={product.imageProfile} alt="asdas" srcset="" />
-        
-          <p>Nombre: {product.name}</p>
-          <p>Precio: ${product.price}</p>
-       
-        </div>
-      )}
+      <DetalleProducto />
     </div>
   );
 };
